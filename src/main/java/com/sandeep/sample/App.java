5a099ba7d5359a146ebdd0d4d466610700450194
sandeepreddy <sandeepreddybommineni2605@gmail.com>
2022-12-06 21:47:52 +0530

    Demo for Team

---
+++ src/main/java/com/sandeep/sample/App.java
@@ -0,0 +1,13 @@
+package com.sandeep.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is SE project" );
+    }
+}
